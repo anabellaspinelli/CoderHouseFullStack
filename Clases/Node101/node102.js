dbcc3d40bc2fc10ab5b5176a89dd36792ca52126
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-07-25 16:11:29 -0300

    Adding super solution with functions returning functions D:

--- Clases/Node101/node102.js
+++ Clases/Node101/node102.js
@@ -12,7 +12,34 @@ for (var i = 2; i < process.argv.length; i++) {
 	});
 }*/
 
-var fs = require('fs');
+//////////////////////////////////////////////////////////////////////////////////////////
+
+/*var contador = 0;
+
+for (var i = 0; i < 10; i++) {
+	setTimeout((function(param) {
+		return function () {
+			console.log(param);
+		} 
+	})(i), getRandomInt(1000, 4000));
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function createFunction() {
+	var args = Array.prototype.slice.call(arguments);
+	return function createdFunction() {
+		console.log(args);
+	};
+}
+
+var f = createFunction('one', 'two');*/
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+/*var fs = require('fs');
 var concatString = '';
 var filesContents = {};
 var count = 2;
@@ -25,7 +52,60 @@ for (var i = 2; i < process.argv.length; i++) {
 			for (var i = 2; i < count; i++) {
 				concatString += filesContents[i];
 			};
+			console.log(filesContents);
 			console.log(concatString);
 		}
 	});
+}*/
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+var fs = require('fs');
+var curFilename;
+var processed = 0;
+var filesContent = [];
+var filenames = process.argv.slice(2);
+
+function createReadHandler(pos) {
+	return function (err, content) {
+		var result;
+
+		processed++;
+
+		if (err) {
+			console.log(err);
+			return;
+		}
+		
+		filesContent[pos] = content;
+
+		if (processed === filenames.length) {
+			result = filesContent.reduce(function(prevContent, curContent) {
+				return prevContent + curContent;
+			}, '');
+			console.log(result);
+		}
+	};
 }
+
+for (var i = 0; i < filenames.length; i++) {
+	curFilename = filenames[i];
+
+	fs.readFile(curFilename, 'UTF8', createReadHandler(i));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
