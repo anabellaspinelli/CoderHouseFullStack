99d00e880ca718b7739e4544871e8b69560bc181
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-07-30 00:13:35 -0300

    Update

--- TPs/07/vocalesAMayusculas.js
+++ TPs/07/vocalesAMayusculas.js
@@ -3,35 +3,34 @@ var path = require('path');
 var filename = process.argv[2];
 
 fs.readFile(filename, 'utf8', function(err, data) {
-	if (err) {
-		console.log(err);
-		return;
-	}
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-	fs.writeFile(createNewFileName(filename), vowelsToUppercase(data), 'utf8', function(err) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-	});
+    fs.writeFile(createNewFileName(filename), vowelsToUppercase(data), 'utf8', function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
 });
 
 function vowelsToUppercase(string) {
-	var result = '';
-	var charArray = string.split('');
+    var result = '';
+    var charArray = string.split('');
 
-	charArray.forEach(function(letter) {
-		if (/[aeiou]/.test(letter)) {
+    charArray.forEach(function(letter) {
+        if (/[aeiou]/.test(letter)) {
             result = result + letter.toUpperCase();
         } else {
-        	result = result + letter;
+            result = result + letter;
         }
-	});
-
-	return result;
+    });
+    return result;
 }
 
 function createNewFileName(filename) {
-	var extension = path.extname(filename);
-	return filename.replace(extension, 'UpperCase' + extension);
-}	
+    var extension = path.extname(filename);
+    return filename.replace(extension, 'UpperCase' + extension);
+}
