9a065e32a135daa0a4e63cbe419579dd83fa1d22
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-08-21 00:11:02 -0300

    Caching DOM elements instead of looking them up each time.

--- TPFinal/public/javascripts/publish.js
+++ TPFinal/public/javascripts/publish.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-    var category = $('#category');
-    var title = $('#title');
-    var description = $('#description');
-    var price = $('#price');
-    var currency = $('#currency');
-    /*MISSING IMAGES*/
-    var name = $('#name');
-    var email = $('#email');
-    var phone = $('#phone');
-    var province = $('#province');
-    var city = $('#city');
-
     var validationRules = {
         category: {
             required: true,
@@ -56,6 +44,7 @@ $(document).ready(function() {
         var item;
 
         e.preventDefault();
+        getFormControls();
         item = extractItemFromForm();
 
         $.ajax({
@@ -76,19 +65,83 @@ $(document).ready(function() {
         });
     });
 
+    function getFormControls() {
+        var $category,
+            $title,
+            $description,
+            $price,
+            $currency,
+            $name,
+            $email,
+            $phone,
+            $province,
+            $city;
+
+        if (!getFormControls.$category) {
+            $category = $('#category');
+        }
+
+        if (!getFormControls.$title) {
+            $title = $('#title');
+        }
+
+        if (!getFormControls.$description) {
+            $description = $('#description');
+        }
+
+        if (!getFormControls.$price) {
+            $price = $('#price');
+        }
+
+        if (!getFormControls.$currency) {
+            $currency = $('#currency');
+        }
+
+        if (!getFormControls.$name) {
+            $name = $('#name');
+        }
+
+        if (!getFormControls.$email) {
+            $email = $('#email');
+        }
+
+        if (!getFormControls.$phone) {
+            $phone = $('#phone');
+        }
+
+        if (!getFormControls.$province) {
+            $province = $('#province');
+        }
+
+        if (!getFormControls.$city) {
+            $city = $('#city');
+        }
+
+        getFormControls.$category = $category;
+        getFormControls.$title = $title;
+        getFormControls.$description = $description;
+        getFormControls.$price = $price;
+        getFormControls.$currency = $currency;
+        getFormControls.$name = $name;
+        getFormControls.$email = $email;
+        getFormControls.$phone = $phone;
+        getFormControls.$province = $province;
+        getFormControls.$city = $city;
+    }
+
     function extractItemFromForm() {
         return {
-            category: category.val(),
-            title: title.val(),
-            description: description.val(),
-            price: Number(price.val()),
-            currency: currency.val(),
+            category: getFormControls.$category.val(),
+            title: getFormControls.$title.val(),
+            description: getFormControls.$description.val(),
+            price: Number(getFormControls.$price.val()),
+            currency: getFormControls.$currency.val(),
             seller: {
-                name: name.val(),
-                email: email.val(),
-                phone: phone.val(),
-                province: province.val(),
-                city: city.val()
+                name: getFormControls.$name.val(),
+                email: getFormControls.$email.val(),
+                phone: getFormControls.$phone.val(),
+                province: getFormControls.$province.val(),
+                city: getFormControls.$city.val()
             }
         }
     }
