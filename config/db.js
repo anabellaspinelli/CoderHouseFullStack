1afc1dcf6722202ec78ab5ef13c179d7ced9f2cf
Anabella Spinelli <anabellaspinelli@gmail.com>
2015-08-24 12:47:35 -0300

    More openshift fixes?

--- config/db.js
+++ config/db.js
@@ -1,12 +1,8 @@
 var credentials = {
     user: 'admin',
     pwd: '123456',
-    url: (connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;) ||
-        'mongodb://admin:123456@localhost:27017/instroo'
+    url: (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) ||
+    'mongodb://admin:123456@localhost:27017/instroo'
 }
 
 module.exports = credentials;
