9bd43dc819aa02eb4c310c2f061d7d2f7c3b576b
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2015-08-23 22:35:46 -0300

    usar las variables de openshift si están presentes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 var credentials = {
     user: 'admin',
     pwd: '123456',
-    url: 'mongodb://admin:123456@localhost:27017/instroo'
+    url: (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) ||
+    'mongodb://admin:123456@localhost:27017/instroo'
 }
 
 module.exports = credentials;
